95959a7f6034599c00aa5839d3647fe7230be045
radostevdk <radostevdk@silaunion.ru>
2023-02-18 19:12:01 +0700

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -33,6 +34,11 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "./src/components/app.css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "./public/photos"), to: path.resolve(__dirname, "./photos") },
+      ],
     }),
   ],
 };
